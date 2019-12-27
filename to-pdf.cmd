e359395e976a37e6eb115aa701b94b0b557e125c
Gogs <gogs@fake.local>
2019-12-27 11:41:11 +0500

    Some changes

--- to-pdf.cmd
+++ to-pdf.cmd
@@ -1,3 +1,15 @@
 @echo off
-xsltproc -output %TMP:\=/%/%~n1%.fo C:/DocBook/espd-docbook5/espd/espd.xsl %~pn1.xml
-rem fop -l en -c C:/DocBook/espd-docbook5/fop.xml %TMP:\=/%/%~n1.fo %~n1.pdf
+set tmpfile=%TMP:\=/%/%~n1%.fo
+
+rem set xslfile=C:/DocBook/espd-docbook5/espd/espd.xsl
+set xslfile=C:\Users\FedorchenkoMI\Documents\MyFirstBook\custom.xsl
+set xslfile=%xslfile:\=/%
+
+set xmlfile=%~dpn1.xml
+set xmlfile=%xmlfile:\=/%
+
+set pdffile=%~dp1..\%~n1.pdf
+set pdffile=%pdffile:\=/%
+
+xsltproc --output %tmpfile% %xslfile% %xmlfile%
+fop -l en -c C:/DocBook/espd-docbook5/fop.xml %tmpfile% %pdffile%
